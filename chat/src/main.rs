98206c9fa91f259359fc66b001030bfc3f11b949
Changyou Liang <changyou.liang@shopee.com>
2024-11-11 09:22:45 +0800

    chat

---
+++ chat/src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use axum::Router;
+use axum::routing::get;
+use tokio::sync::broadcast;
+use tracing_subscriber;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=trace", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(1000);
+
+    let app_state = Arc::new(AppState { user_set, tx });
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/websocket", get(websocket_handler))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() {}
+
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<AppState>>,
+) {}
